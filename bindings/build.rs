c113ff21e9e394e557f1e6b81fc1493a2c065a17
Michael Bikovitsky <biko@bikodbg.com>
2021-10-16 12:56:32 +0300

    Switch to using EM_ENABLESEARCHWEB

--- bindings/build.rs
+++ bindings/build.rs
@@ -5,21 +5,15 @@ fn main() {
         Windows::Win32::UI::WindowsAndMessaging::CallNextHookEx,
         Windows::Win32::UI::WindowsAndMessaging::MessageBoxW,
         Windows::Win32::UI::WindowsAndMessaging::GetClassNameW,
-        Windows::Win32::UI::WindowsAndMessaging::AppendMenuW,
-        Windows::Win32::UI::WindowsAndMessaging::GetMenuItemCount,
-        Windows::Win32::UI::WindowsAndMessaging::GetMenuItemID,
+        Windows::Win32::UI::WindowsAndMessaging::SendMessageW,
 
         Windows::Win32::System::Diagnostics::Debug::OutputDebugStringW,
 
-        Windows::Win32::System::LibraryLoader::GetModuleHandleW,
-        Windows::Win32::System::LibraryLoader::GetProcAddress,
-
         Windows::Win32::System::SystemServices::IMAGE_DOS_HEADER,
 
-        Windows::Win32::UI::WindowsAndMessaging::CWPRETSTRUCT,
+        Windows::Win32::UI::WindowsAndMessaging::CWPSTRUCT,
 
-        Windows::Win32::UI::WindowsAndMessaging::HC_ACTION,
-        Windows::Win32::UI::WindowsAndMessaging::WM_INITMENUPOPUP,
-        Windows::Win32::UI::WindowsAndMessaging::WM_INITMENU,
+        Windows::Win32::UI::Controls::WM_CONTEXTMENU,
+        Windows::Win32::UI::Controls::EM_ENABLESEARCHWEB,
     };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,16 @@ mod windows_hook;
 
 use std::panic::catch_unwind;
 
-use anyhow::{ensure, Context};
+use anyhow::Context;
 use bindings::Windows::Win32::{
     Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
     System::Diagnostics::Debug::OutputDebugStringW,
-    UI::WindowsAndMessaging::{
-        AppendMenuW, CallNextHookEx, GetMenuItemCount, GetMenuItemID, MessageBoxW, CWPRETSTRUCT,
-        HMENU, MB_ICONERROR, MB_OK, MF_DISABLED, MF_SEPARATOR, MF_STRING, WH_CALLWNDPROCRET,
-        WM_INITMENU, WM_INITMENUPOPUP,
+    UI::{
+        Controls::{EM_ENABLESEARCHWEB, WM_CONTEXTMENU},
+        WindowsAndMessaging::{
+            CallNextHookEx, MessageBoxW, SendMessageW, CWPSTRUCT, MB_ICONERROR, MB_OK,
+            WH_CALLWNDPROC,
+        },
     },
 };
 use widestring::U16CStr;
@@ -20,16 +22,16 @@ use crate::{
     windows_hook::WindowsHook,
 };
 
-extern "system" fn bong_hook(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+extern "system" fn call_wndproc_hook(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     let _result = catch_unwind(|| {
         if code < 0 {
             return;
         }
 
-        let message_info = unsafe { (lparam.0 as *const CWPRETSTRUCT).as_ref() };
+        let message_info = unsafe { (lparam.0 as *const CWPSTRUCT).as_ref() };
 
         if let Some(message_info) = message_info {
-            if let Err(error) = process_window_message(message_info) {
+            if let Err(error) = process_call_wndproc_hook(message_info) {
                 unsafe {
                     OutputDebugStringW(format!("bong | {:?}", error));
                 }
@@ -40,66 +42,26 @@ extern "system" fn bong_hook(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESU
     unsafe { CallNextHookEx(None, code, wparam, lparam) }
 }
 
-fn process_window_message(message_info: &CWPRETSTRUCT) -> anyhow::Result<()> {
-    // https://stackoverflow.com/questions/1346473/add-item-to-the-default-textbox-context-menu
-    // https://stackoverflow.com/questions/46437708/inherit-or-subclass-textbox-to-disable-paste-on-textbox
-
-    const WM_UAHINITMENU: u32 = 0x93;
-
-    let hmenu = match message_info.message {
-        WM_UAHINITMENU => {
-            ensure!(message_info.lParam.0 != 0, "LPARAM is NULL");
-            HMENU(unsafe { *(message_info.lParam.0 as *const isize) })
-        }
-        WM_INITMENU | WM_INITMENUPOPUP => HMENU(message_info.wParam.0 as _),
-        _ => return Ok(()),
-    };
-    ensure!(hmenu.0 != 0, "HMENU is NULL");
+fn process_call_wndproc_hook(message_info: &CWPSTRUCT) -> anyhow::Result<()> {
+    if message_info.message != WM_CONTEXTMENU {
+        return Ok(());
+    }
 
     let owner_class_name = unsafe { get_class_name(message_info.hwnd)? };
     if owner_class_name.to_string_lossy().to_lowercase() != "edit" {
         return Ok(());
     }
 
-    let max_id = unsafe { find_max_menu_id(hmenu)? };
-
     unsafe {
-        AppendMenuW(hmenu, MF_SEPARATOR, 0, None)
-            .ok()
-            .with_context(|| "Failed adding separator")?;
-    }
-
-    unsafe {
-        AppendMenuW(
-            hmenu,
-            MF_DISABLED | MF_STRING,
-            (max_id + 1) as _,
-            "Search with Bing...",
-        )
-        .ok()
-        .with_context(|| "Failed adding new menu item")?;
+        SendMessageW(
+            message_info.hwnd,
+            EM_ENABLESEARCHWEB,
+            WPARAM(true.into()),
+            LPARAM(0),
+        );
     }
 
     Ok(())
-}
-
-unsafe fn find_max_menu_id(hmenu: HMENU) -> anyhow::Result<u32> {
-    let menu_items = GetMenuItemCount(hmenu);
-    ensure!(menu_items >= 0, "GetMenuItemCount failed");
-
-    let mut max_id = 0u32;
-    for index in 0..menu_items {
-        let id = GetMenuItemID(hmenu, index);
-        if id == u32::MAX {
-            // It's a submenu
-            continue;
-        }
-        if id > max_id {
-            max_id = id;
-        }
-    }
-
-    Ok(max_id)
 }
 
 #[no_mangle]
@@ -135,7 +97,14 @@ fn do_hook(command_line: *const u16) -> anyhow::Result<()> {
         .parse()
         .with_context(|| format!("Invalid TID: {}", command_line))?;
 
-    let _hook = unsafe { WindowsHook::set(WH_CALLWNDPROCRET, Some(bong_hook), image_base(), tid)? };
+    let _edit_menu_hook = unsafe {
+        WindowsHook::set(
+            WH_CALLWNDPROC,
+            Some(call_wndproc_hook),
+            image_base(),
+            tid,
+        )?
+    };
 
     Ok(())
 }
